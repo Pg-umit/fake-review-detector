f239878469be8bcb9acf54f76babc63395f73ad9
Pg-umit <priyankanss.umit@gmail.com>
2025-04-21 12:05:07 +0530

    Fix: Bind to 0.0.0.0 for Render

--- app.py
+++ app.py
@@ -25,7 +25,8 @@ def home():
     return render_template('index.html', result=result, review=review)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=10000)
+
 
 
 
